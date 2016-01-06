b73b00abec02767666e286fab41a74c5fddcef9e
everyx <lunt.luo@gmail.com>
2016-01-06 10:11:23 +0800

    fix 360 & header info

--- message.user.js
+++ message.user.js
@@ -1,31 +1,32 @@
 // ==UserScript==
-// @id             message.alibaba.everyx.in
+// @id             message.user.js@everyx
 // @name           阿里询盘界面优化
 // @version        1.0
-// @namespace      
-// @author         
-// @description    
+// @namespace      http://userscript.everyx.in/message.user.js
+// @author         everyx
+// @description    阿里询盘界面优化
 // @include        http://message.alibaba.com/message/default.htm#feedback/detail/all/*
 // @run-at         window-load
+// @updateURL      https://raw.githubusercontent.com/everyx/AliTools/master/message.user.js
 // ==/UserScript==
 
 main();
 
-const WIDTH_TUNING = 1;
+const WIDTH_TUNING = 2;
 
 function main() {
   setTimeout(addCenterDetailToogle(), 3000);
 }
 
 function addCenterDetailToogle() {
-  
+
   var resizeHandle = document.querySelector('.aui-detail-resize-handler');
   var detailCenter = document.querySelector('.aui-detail-center');
   var detailRight = document.querySelector('.aui-detail-right');
-  
+
   var rightWidth = detailRight.offsetWidth;
   var centerWidth = detailCenter.offsetWidth;
-  
+
   resizeHandle.ondblclick = function() {
     if (detailCenter.style.display === "none") {
       display = "";
@@ -34,10 +35,10 @@ function addCenterDetailToogle() {
       display = "none";
       width = rightWidth + centerWidth - WIDTH_TUNING;
     }
-    
+
     detailRight.style.width = width + "px";
     detailCenter.style.display = display;
-    
+
     rightWidth = width;
   };
 }
