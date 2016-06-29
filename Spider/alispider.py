df92ed1fad0bd7dbe087a7ee5294ce8a33ed8e13
everyx <lunt.luo@gmail.com>
2016-06-29 10:57:13 +0800

    update gitignore

---
+++ Spider/alispider.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from api import Spider
+
+if __name__ == '__main__':
+    spider = Spider()
+
+    # spider.export_keywords('belt')
+
+    # spider.export_keywords('wallet')
+    # spider.export_keywords('sleeve')
+    # spider.export_keywords('billfold')
+    # spider.export_keywords('holder')
+    spider.export_keywords('case')
+
+    # spider.export_keywords('bag')
+    # spider.export_keywords('handbag')
+    # spider.export_keywords('clutch')
+    # spider.export_keywords('tote')
+    # spider.export_keywords('satchel')
+    # spider.export_keywords('backpack')
+    # spider.export_keywords('saddlebag')
+    # spider.export_keywords('purse')
+    # spider.export_keywords('pouch')
+    # spider.export_keywords('briefcase')
+
+
+    #
+    # spider.export_keywords('key ring')
+    # spider.export_keywords('key chain')
+    # spider.export_keywords('key fob')
+    #
+    # spider.export_keywords('bracelet')
+    # spider.export_keywords('bangle')
+    # spider.export_keywords('wrist strap')
+    # spider.export_keywords('wrist ring')
+    # spider.export_keywords('wrist band')
+    # spider.export_keywords('wristband')
+    #
+    # spider.export_keywords('leash')
+    # spider.export_keywords('collar')
+    #
+    # spider.export_keywords('leather bookmark')
+    # spider.export_keywords('button')
+    # spider.export_keywords('organizer')
--- Spider/api.py
+++ Spider/api.py
@@ -1,28 +1,133 @@
-{
-    'rank': {
-        'top1_product_id'      : str,
-        'top1_product_position': float,
-        'product_position'     : float,
-        'is_selection_poduct'  : bool
-    },
-    'product': {
-        'keyword'           : list,
-        'product_owner'     : str,
-        'product_no'        : str,
-        'product_id'        : str,
-        'is_window_product' : bool,
-        'is_ydt_product'    : bool
-    },
-    'keyword': {
-        "keyword_company_count"         : int,
-        "keyword_window_products_count" : int,
-        "keyword_pv_rank"               : int,
-        "is_p4p_keyword"                : bool
-    }
-}
+import requests
+import time
+import random
+from datetime import datetime
+from http.cookiejar import MozillaCookieJar
+from dateutil.relativedelta import relativedelta
+import os
+import csv
+import sys
 
-class Rank(object):
-    """docstring for Rank"""
-    def __init__(self, arg):
-        super(Rank, self).__init__()
-        self.arg = arg
+class Spider(object):
+    """docstring for AliSpider"""
+    def __init__(self):
+        self.session = self.create_session("./cookies.txt")
+
+    def create_session(self, cookies_file_path = "./cookies.txt"):
+        s = requests.Session()
+
+        cj = MozillaCookieJar(cookies_file_path)
+        cj.load(ignore_discard=True, ignore_expires=True)
+        for cookie in cj:
+            cookie.expires = time.time() + 14 * 24 * 3600
+        s.cookies = cj
+
+        s.headers = {
+            'User-Agent':      'Mozilla/5.0 (X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0',
+            'Accept':          '*/*',
+            'Accept-Language': 'zh-CN,en-US;q=0.7,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate',
+            'Connection':      'keep-alive'
+        }
+
+        s.get("http://i.alibaba.com/index.htm")
+        self.delay()
+
+        return s
+
+    def search_keywords(self, keyword, page_no=1, page_size=10):
+        api_url = "http://hz-mydata.alibaba.com/industry/.json?action=CommonAction&iName=searchKeywords"
+        request_data = {
+            'keywords':   keyword,
+            'pageNO':     str(page_no),
+            'pageSize':   str(page_size),
+            'orderBy':    'srh_pv_this_mon',
+            'orderModel': 'desc'
+        }
+        self.session.headers.update({
+            'Host':             'hz-mydata.alibaba.com',
+            'Content-Type':     'application/x-www-form-urlencoded; charset=UTF-8',
+            'X-Requested-With': 'XMLHttpRequest',
+            'Referer':          'http://hz-mydata.alibaba.com/industry/keywords.htm'
+        })
+        rsp = self.session.post( api_url, data = request_data ).json()
+        result = None
+        if rsp.get('successed', False):
+            result = rsp['value']
+
+        self.delay()
+        return result
+
+    def export_keywords(self, keyword, export_path='./csv/'):
+        keywords = list()
+
+        data_dump_path = './tmp/' + keyword + '.dump'
+        if os.path.isfile(data_dump_path):
+            with open(data_dump_path, 'r') as data_file:
+                keywords = [ast.literal_eval(x) for x in data_file.readlines() if x is not None]
+
+        page_size = 10
+        page_no = int(len(keywords) / page_size) + 1
+
+        with open(data_dump_path, 'a') as dump_file:
+            while True:
+                l = self.search_keywords(keyword, page_no=page_no, page_size=page_size)
+
+                if len(keywords) == l['total']:
+                    break
+
+                data = l['data']
+                for data_item in data:
+                    dump_file.write("%s\n" % data_item)
+                dump_file.flush()
+                keywords.extend(data)
+
+                sys.stdout.write('\r[%s] %d - %d' % (keyword, len(keywords), l['total']))
+                sys.stdout.flush()
+                page_no += 1
+
+        headers = [
+            ("keywords",          "关键词"),
+            ("repeatKeyword",     "变体词"),
+            ("company_cnt",       "卖家竞争度"),
+            ("showwin_cnt",       "橱窗数"),
+            ("srh_pv_this_mon",   self.months_ago_str(0)),
+            ("srh_pv_last_1mon",  self.months_ago_str(1)),
+            ("srh_pv_last_2mon",  self.months_ago_str(2)),
+            ("srh_pv_last_3mon",  self.months_ago_str(3)),
+            ("srh_pv_last_4mon",  self.months_ago_str(4)),
+            ("srh_pv_last_5mon",  self.months_ago_str(5)),
+            ("srh_pv_last_6mon",  self.months_ago_str(6)),
+            ("srh_pv_last_7mon",  self.months_ago_str(7)),
+            ("srh_pv_last_8mon",  self.months_ago_str(8)),
+            ("srh_pv_last_9mon",  self.months_ago_str(9)),
+            ("srh_pv_last_10mon", self.months_ago_str(10)),
+            ("srh_pv_last_11mon", self.months_ago_str(11)),
+            ("isP4pKeyword",      "P4P关键词"),
+            ("yyyymm",            "更新时间")
+        ]
+        data = list()
+        for item in keywords:
+            data_item = dict()
+            for x in headers:
+                data_item[x[0]] = item.get(x[0], "")
+            data.append(data_item)
+
+        export_filename = os.path.join(export_path, keyword + '.csv')
+        with open(export_filename, 'w', encoding='utf-8-sig') as export_file:
+            dict_writer = csv.DictWriter(export_file, fieldnames = [x[0] for x in headers])
+            dict_writer.writerow(dict(headers))
+            dict_writer.writerows(data)
+
+    def months_ago_str(self, months):
+        time = datetime.now() - relativedelta(months=months)
+        return time.strftime('%y年%m月搜索热度')
+
+    def query_products_list(self):
+        pass
+
+    def rank_search(self):
+        pass
+
+    def delay(self, seconds=10):
+        time.sleep(random.uniform(1, seconds))
