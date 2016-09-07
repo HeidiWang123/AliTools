887fcf87c8b1e625c53fde91c604e73e8ab18a4a
everyx <lunt.luo@gmail.com>
2016-09-08 00:41:34 +0800

    fix #15

--- Spider/ali_spider/spider.py
+++ Spider/ali_spider/spider.py
@@ -3,25 +3,70 @@
 """
 """
 
+import argparse
 from crawer import Crawer
 from database import Database
 from generator import CSV_Generator
 
-
-if __name__ == "__main__":
+def craw(craw_args):
     database = Database()
     try:
         ali_crawer = Crawer(database=database)
-        # ali_crawer.craw_products()
-        ali_crawer.craw_p4p()
-        # keywords = database.get_craw_keywords()
-        # ali_crawer.craw_keywords(keywords=keywords)
-        # ali_crawer.craw_keywords_category(index=9811)
-        # ali_crawer.craw_rank(keywords=keywords)
-        csv_generator = CSV_Generator(database=database)
-        csv_generator.generate_p4p_csv()
-        # csv_generator.generate_overview_csv(keywords=keywords)
-    except Exception as e:
+        actions = {
+            'products': ali_crawer.craw_products,
+            'keywords': ali_crawer.craw_keywords,
+            'rank': ali_crawer.craw_rank,
+            'p4p': ali_crawer.craw_p4p,
+        }
+        for arg in craw_args.action:
+            action_args = {
+                'products': {},
+                'keywords': {'keywords': database.get_craw_keywords()},
+                'rank': {'keywords': database.get_craw_keywords()},
+                'p4p': {},
+            }
+            actions.get(arg)(**action_args.get(arg))
+    except Exception:
         raise
     finally:
         database.close()
+
+def generate(generate_args):
+    database = Database()
+    try:
+        csv_generator = CSV_Generator(database=database)
+        actions = {
+            'overview': csv_generator.generate_overview_csv,
+            'keywords': csv_generator.generate_keywords_csv,
+            'p4p': csv_generator.generate_p4p_csv,
+        }
+        for arg in generate_args.action:
+            action_args = {
+                'overview': {'keywords': database.get_craw_keywords()},
+                'keywords': {'keywords': database.get_craw_keywords()},
+                'p4p': {},
+            }
+            actions.get(arg)(**action_args.get(arg))
+    except Exception:
+        raise
+    finally:
+        database.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    craw_parser = subparsers.add_parser('craw', help="craw data and save to database")
+    generate_parser = subparsers.add_parser('generate', help="generate csv file")
+    craw_parser.add_argument(
+        '-a', '--action', action='append', choices=['products', 'keywords', 'rank', 'p4p'],
+        help='craw specific type items'
+    )
+    generate_parser.add_argument(
+        '-a', '--action', action='append', choices=['overview', 'keywords', 'p4p'],
+        help='generate specific csv file'
+    )
+    craw_parser.set_defaults(func=craw)
+    generate_parser.set_defaults(func=generate)
+    args = parser.parse_args()
+    args.func(args)
